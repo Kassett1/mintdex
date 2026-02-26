94e5bfa39e92df0612322c16adc29938ef58e418
Kassett1 <killian.touchard1@gmail.com>
2026-02-26 15:22:19 +0100

    ajout de wishlist sur la page card

--- src/Controller/CardPageController.php
+++ src/Controller/CardPageController.php
@@ -3,6 +3,7 @@ namespace App\Controller;
 
 use App\Repository\CardRepository;
 use App\Repository\UserCardRepository;
+use App\Repository\WishlistCardRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -10,7 +11,7 @@ use Symfony\Component\Routing\Attribute\Route;
 final class CardPageController extends AbstractController
 {
     #[Route('/card/{slug}', name: 'app_card_page')]
-    public function index(CardRepository $cr, UserCardRepository $ucr, string $slug): Response
+    public function index(CardRepository $cr, UserCardRepository $ucr, string $slug, WishlistCardRepository $wc): Response
     {
         $card = $cr->findOneBy(['slug' => $slug]);
 
@@ -20,9 +21,18 @@ final class CardPageController extends AbstractController
             ? $ucr->findBy(['user' => $user, 'card' => $card])
             : null;
 
+        $wishlistCards = [];
+        if ($user) {
+            $userCards = $wc->findAll();
+            foreach ($userCards as $w) {
+                $wishlistCards[] = $w->getCard()->getId();
+            }
+        }
+
         return $this->render('card_page/index.html.twig', [
             'card'  => $card,
             'cards' => $cards,
+            'wishlistCards' => $wishlistCards,
         ]);
     }
 }
